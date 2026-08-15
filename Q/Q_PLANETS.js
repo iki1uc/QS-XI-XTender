36869c64fc98fd5a236c2cbc81b946524ffc5dee
iki1uc <entertrainer@web.de>
2026-08-15 10:54:52 +0200

    Refactor Q_PLANETS to PLANETS with simplified structure

--- Q/Q_PLANETS.js
+++ Q/Q_PLANETS.js
@@ -1,23 +1,5 @@
-export const Q_PLANETS = {
-    tokio: {
-        name: "Tokio-X4",
-        baseR: 2268,
-        r_rel: 9.33,
-        v_rel: 0.021,
-        T_rel: 108000
-    },
-    hh: {
-        name: "HH-XI",
-        baseR: 243,
-        r_rel: 1.00,
-        v_rel: 0.064,
-        T_rel: 3780
-    },
-    corlu: {
-        name: "Corlu-IX",
-        baseR: 756,
-        r_rel: 3.11,
-        v_rel: 0.036,
-        T_rel: 20800
-    }
+export const PLANETS = {
+    tokio: { r: 2268 },
+    corlu: { r: 756 },
+    hh: { r: 243 }
 };
