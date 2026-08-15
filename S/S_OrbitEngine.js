ab84b426d2960183db0ea6538e8254e418f8725b
iki1uc <entertrainer@web.de>
2026-08-15 09:46:18 +0200

    Implement S_OrbitEngine class for orbital calculations

--- S/S_OrbitEngine.js
+++ S/S_OrbitEngine.js
@@ -1,1 +1,20 @@
+export class S_OrbitEngine {
+    constructor(coreMass){
+        this.G = 6.674e-11;
+        this.coreMass = coreMass;
+        this.t = 0;
+    }
+
+    orbit(p){
+        this.t += 0.01;
+        const r = p.baseR * 1.496e11;
+        const omega = Math.sqrt((this.G * this.coreMass) / (r*r*r));
+        const angle = this.t * omega;
+
+        return {
+            x: Math.cos(angle) * r,
+            y: Math.sin(angle) * r
+        };
+    }
+}
 
