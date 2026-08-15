b8a68ab954903b6141ecc281f2a9c3dd31718dde
iki1uc <entertrainer@web.de>
2026-08-15 10:54:35 +0200

    Refactor Q_CORE_XI to use CORE_CONSTANTS

--- Q/Q_CORE_XI.js
+++ Q/Q_CORE_XI.js
@@ -1,17 +1,7 @@
+import { CORE_CONSTANTS } from "../CORE/CORE_constants.js";
+
 export const Q_CORE_XI = {
-    G: 6.674e-11,
-    c: 299792458,
-    mass: 1.989e36,
-
-    schwarzschild(){
-        return (2 * this.G * this.mass) / (this.c ** 2);
-    },
-
-    corePackage(){
-        return {
-            mass: this.mass,
-            rs: this.schwarzschild()
-        };
-    }
+    version: "XI",
+    baseR: 1000,
+    constants: CORE_CONSTANTS
 };
-
