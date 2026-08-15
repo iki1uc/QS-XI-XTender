695671862c47c3cd822cf7998d012341d2be0550
iki1uc <entertrainer@web.de>
2026-08-15 09:54:53 +0200

    Create Q_PLANETS.js

---
+++ Q/Q_PLANETS.js
@@ -0,0 +1,23 @@
+export const Q_PLANETS = {
+    tokio: {
+        name: "Tokio-X4",
+        baseR: 2268,
+        r_rel: 9.33,
+        v_rel: 0.021,
+        T_rel: 108000
+    },
+    hh: {
+        name: "HH-XI",
+        baseR: 243,
+        r_rel: 1.00,
+        v_rel: 0.064,
+        T_rel: 3780
+    },
+    corlu: {
+        name: "Corlu-IX",
+        baseR: 756,
+        r_rel: 3.11,
+        v_rel: 0.036,
+        T_rel: 20800
+    }
+};
