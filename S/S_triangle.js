7145cf40e6bba0f31cf8e040a7921e7ac96e9a3a
iki1uc <entertrainer@web.de>
2026-08-15 11:09:51 +0200

    Refactor triangle function to use descriptive keys

--- S/S_triangle.js
+++ S/S_triangle.js
@@ -1,8 +1,7 @@
 export function triangle(A, B, C){
-    const dAB = Math.hypot(A.x - B.x, A.y - B.y);
-    const dBC = Math.hypot(B.x - C.x, B.y - C.y);
-    const dCA = Math.hypot(C.x - A.x, C.y - A.y);
-
-    return { dAB, dBC, dCA };
+    return {
+        AB: Math.hypot(A.x - B.x, A.y - B.y),
+        BC: Math.hypot(B.x - C.x, B.y - C.y),
+        CA: Math.hypot(C.x - A.x, C.y - A.y)
+    };
 }
-
