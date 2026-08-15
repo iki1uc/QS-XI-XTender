1dc5b50a69c8ea82870815e9545fe17d6e163758
iki1uc <entertrainer@web.de>
2026-08-15 10:55:30 +0200

    Implement timeDilation function using CORE_CONSTANTS

--- R/R_timeDilation.js
+++ R/R_timeDilation.js
@@ -1,1 +1,7 @@
+import { CORE_CONSTANTS } from "../CORE/CORE_constants.js";
+
+export function timeDilation(mass, r){
+    const rs = 2 * CORE_CONSTANTS.G * mass / CORE_CONSTANTS.c**2;
+    return Math.sqrt(1 - rs / r);
+}
 
