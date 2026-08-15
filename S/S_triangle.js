b35cdaea97041f35d759aa15e3ac465f3e51a148
iki1uc <entertrainer@web.de>
2026-08-15 10:56:30 +0200

    Add triangle function to calculate distances between points

--- S/S_triangle.js
+++ S/S_triangle.js
@@ -1,1 +1,8 @@
+export function triangle(A, B, C){
+    const dAB = Math.hypot(A.x - B.x, A.y - B.y);
+    const dBC = Math.hypot(B.x - C.x, B.y - C.y);
+    const dCA = Math.hypot(C.x - A.x, C.y - A.y);
+
+    return { dAB, dBC, dCA };
+}
 
