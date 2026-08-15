d34e895c34edb14e7f67d41ed0fb9a7bcf1f79ff
iki1uc <entertrainer@web.de>
2026-08-15 11:14:01 +0200

    Add drawTriangle function to UI_Triangle.js

--- UI/UI_Triangle.js
+++ UI/UI_Triangle.js
@@ -1,1 +1,8 @@
-
+export function drawTriangle(ctx, A, B, C){
+    ctx.beginPath();
+    ctx.moveTo(A.x, A.y);
+    ctx.lineTo(B.x, B.y);
+    ctx.lineTo(C.x, C.y);
+    ctx.closePath();
+    ctx.stroke();
+}
