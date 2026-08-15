5adf2798e6d23015a9a87467393eee5801112789
iki1uc <entertrainer@web.de>
2026-08-15 10:56:46 +0200

    Add realMeasure function with curvature and timeDilation

--- S/S_realmeasure.js
+++ S/S_realmeasure.js
@@ -1,1 +1,10 @@
+import { curvature } from "../R/R_curvature.js";
+import { timeDilation } from "../R/R_timeDilation.js";
+
+export function realMeasure(mass, r){
+    return {
+        curvature: curvature(mass, r),
+        timedilation: timeDilation(mass, r)
+    };
+}
 
