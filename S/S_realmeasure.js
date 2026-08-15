0addee76a17534bab979b5d0d93508fee1123ca2
iki1uc <entertrainer@web.de>
2026-08-15 11:10:31 +0200

    Add orbitVelocity to realMeasure function

--- S/S_realmeasure.js
+++ S/S_realmeasure.js
@@ -1,10 +1,11 @@
 import { curvature } from "../R/R_curvature.js";
 import { timeDilation } from "../R/R_timeDilation.js";
+import { orbitVelocity } from "../R/R_orbitphysics.js";
 
 export function realMeasure(mass, r){
     return {
         curvature: curvature(mass, r),
+        velocity: orbitVelocity(mass, r),
         timedilation: timeDilation(mass, r)
     };
 }
-
