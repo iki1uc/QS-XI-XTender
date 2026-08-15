12b432f29761ca3faa18fa3336c71e7be221aa3b
iki1uc <entertrainer@web.de>
2026-08-15 10:57:17 +0200

    Update UI_Values.js

--- UI/UI_Values.js
+++ UI/UI_Values.js
@@ -1,1 +1,6 @@
+export function drawValues(ctx, real, golem){
+    ctx.fillStyle = "white";
+    ctx.fillText("Real Krümmung: " + real.curvature, 20, 20);
+    ctx.fillText("Golem Krümmung: " + golem.curvature, 20, 40);
+}
 
