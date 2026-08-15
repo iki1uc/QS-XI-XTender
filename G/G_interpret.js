55126770d95d407500798c4bd3065b0917e81859
iki1uc <entertrainer@web.de>
2026-08-15 10:55:59 +0200

    Update G_interpret.js

--- G/G_interpret.js
+++ G/G_interpret.js
@@ -1,1 +1,10 @@
+import { G_transform } from "./G_transform.js";
+
+export function G_interpret(real){
+    return {
+        curvature: G_transform(real.curvature),
+        velocity: G_transform(real.velocity),
+        timedilation: G_transform(real.timedilation)
+    };
+}
 
