7f8c39f488852ae50013585d1a1cb29f2281097b
iki1uc <entertrainer@web.de>
2026-08-15 09:46:05 +0200

    Update Q_CORE_XI.js

--- Q/Q_CORE_XI.js
+++ Q/Q_CORE_XI.js
@@ -1,1 +1,17 @@
+export const Q_CORE_XI = {
+    G: 6.674e-11,
+    c: 299792458,
+    mass: 1.989e36,
+
+    schwarzschild(){
+        return (2 * this.G * this.mass) / (this.c ** 2);
+    },
+
+    corePackage(){
+        return {
+            mass: this.mass,
+            rs: this.schwarzschild()
+        };
+    }
+};
 
