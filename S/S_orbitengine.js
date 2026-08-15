31cb4906cc2686bad9ce1790430e645d83903384
iki1uc <entertrainer@web.de>
2026-08-15 11:09:43 +0200

    Convert S_OrbitEngine to standalone orbit function
    Refactored S_OrbitEngine class into a standalone orbit function.

--- S/S_orbitengine.js
+++ S/S_orbitengine.js
@@ -1,20 +1,6 @@
-export class S_OrbitEngine {
-    constructor(coreMass){
-        this.G = 6.674e-11;
-        this.coreMass = coreMass;
-        this.t = 0;
-    }
-
-    orbit(p){
-        this.t += 0.01;
-        const r = p.baseR * 1.496e11;
-        const omega = Math.sqrt((this.G * this.coreMass) / (r*r*r));
-        const angle = this.t * omega;
-
-        return {
-            x: Math.cos(angle) * r,
-            y: Math.sin(angle) * r
-        };
-    }
+export function orbit(p){
+    return {
+        x: Math.cos(p.r) * p.r,
+        y: Math.sin(p.r) * p.r
+    };
 }
-
